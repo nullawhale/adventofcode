817e5f72680bf4c3cfe5beebc2e7f5e22be0d11b
NullaWhale <kura98.21@gmail.com>
2018-12-02 13:16:06 +0300

    day 2 - second star

--- src/y2018/day2.rs
+++ src/y2018/day2.rs
@@ -1,6 +1,12 @@
 use std::collections::HashMap;
 
 pub fn run(vs: Vec<String>) {
+    let (twice, thrice) = star1(vs.clone());
+    println!("{} * {} = {}", twice, thrice, twice*thrice);
+    println!("{}", star2(vs.clone()));
+}
+
+fn star1(vs: Vec<String>) -> (usize, usize) {
     let (mut twice, mut thrice) = (0, 0);
     for line in vs.iter() {
         let mut counts = HashMap::new();
@@ -12,5 +18,25 @@ pub fn run(vs: Vec<String>) {
         if counts.values().any(|&x| x == 2) { twice  += 1; }
         if counts.values().any(|&x| x == 3) { thrice += 1; }
     }
-    println!("{} * {} = {}", twice, thrice, twice*thrice);
+
+    return (twice, thrice);
+}
+
+fn star2(vs: Vec<String>) -> String {
+    for one in &vs {
+        for another in &vs {
+            if one.len() == another.len() {
+                if one != another {
+                    let mut diff = 0;
+                    let mut result = String::new();
+                    for x in one.chars().zip(another.chars()) {
+                        if x.0 != x.1 { diff += 1; }
+                        else { result.push(x.0); }
+                    }
+                    if diff == 1 { return result; }
+                }
+            }
+        }
+    }
+    unreachable!();
 }
